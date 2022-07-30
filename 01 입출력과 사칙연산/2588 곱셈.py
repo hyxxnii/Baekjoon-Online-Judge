9d21ad97fbd3d75a1b870c41cb419a41558cf55e
Yeonghyeon Kim <khyeon0819@gmail.com>
2022-07-30 12:19:28 +0900

    2588 곱셉.py

---
+++ 01 입출력과 사칙연산/2588 곱셈.py
@@ -0,0 +1,10 @@
+# (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+# (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+
+num1 = input()
+num2 = input()
+
+print(int(num1)*int(num2[2]))
+print(int(num1)*int(num2[1]))
+print(int(num1)*int(num2[0]))
+print(int(num1)*int(num2))
