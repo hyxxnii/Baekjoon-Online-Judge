d1ecf80b2b001bb5a43595db252ccdd2f368979e
Yeonghyeon Kim <khyeon0819@gmail.com>
2022-08-02 11:08:03 +0900

    2439 별 찍기 - 2.py

---
+++ 03 반복문/2439 별 찍기 - 2.py
@@ -0,0 +1,12 @@
+# 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+# 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
+N = int(input())
+
+for i in range(N, 0, -1):
+    for j in range(N):
+        if j >= i - 1:
+            print("*", end="")
+        else:
+            print(' ', end="")
+    print()
